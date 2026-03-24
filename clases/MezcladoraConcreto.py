cb163a4cc844a66989207af1de6d39f72d1b9bf9
marialuisagtz03@gmail.com <marialuisagtz03@gmail.com>
2026-03-24 10:39:40 -0600

    examen2

--- clases/MezcladoraConcreto.py
+++ clases/MezcladoraConcreto.py
@@ -4,7 +4,10 @@ class EquipoObra:
         def __init__ (self, nombre, identificador, consumoCombustible, capacidadTambor):
             super().__(self,nombre, identificador, consumoCombustible)
             self.capacidadTambor= capacidadTambor
-            
+         
+         
+        def __str__(self):
+            return super().__str()+ " "+ str(self.MezcladorConcreto)   
         def mezclar(self):
             print("Ahora si tu mezcla esta lista", mezclar)
         
--- clases/VibradorConcreto.py
+++ clases/VibradorConcreto.py
@@ -4,6 +4,10 @@ class EquipoOra:
              super().__(self,nombre, identificador, consumoCombustible)
              self.VibradorConcreto= VibradorConcreto
              
+    def __str__(self):
+        return super(). __str__()+" "+ str(self.VibradorConcreto)
+     
+             
     def vibrar(self):
         print("Tu vibrador concreto ya esta")
             
